40ace0caf9c35f6337a96430e30c8201b05b4382
Diego <diegoalencar.jacober@gmail.com>
2025-03-23 21:15:15 -0300

    vou começar fazer

---
+++ previsao-casas-california/main.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import BayesianRidge
+from sklearn.linear_model import Ridge
+from sklearn.svm import SVR
+from sklearn.linear_model import ElasticNet
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+sns.set_style('whitegrid')
+
+# https://www.youtube.com/watch?v=MfpLb0uTmjw
